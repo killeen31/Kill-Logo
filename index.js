589058e976302c7f067d5f5efa320d2d22d27059
killeen31 <j31killeen@gmail.com>
2023-04-03 22:10:58 -0400

    class svg and setting the user text and shape

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const inquirer = require('inquirer');
 const {Circle, Square, Triangle} = require('./lib/shape');
-const filesystem = require('/node_modules/filesystem');
+const filesystem = require('fs');
 
-
+class Svg {
+    constructor(){
+        this.textElement = ''
+        this.shapeElement = ''
+    }
+    render(){
+        return `<svg version=“1.1” xmlns=“http://www.w3.org/2000/svg” width=“300” height=“200">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setTextElement(text,color){
+        this.textElement = `<text x=“150” y=“125" font-size=“60” text-anchor=“middle” fill=“${color}“>${text}</text>`
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
+    }
+}
 
 
 
@@ -14,18 +28,63 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'color',
+        name: 'fontColor',
         message: 'Enter a color',
     },
     {
         type: 'input',
-        name: 'shape',
-        message: 'Enter a shape',
+        name: 'shapeColor',
+        message: 'Enter a shape-color',
     },
     {
-        type: 'list',
-        name: 'pixel-image',
-        message: 'Choose a pixel image',
+        type: 'choices',
+        name: 'shape',
+        message: 'Choose a shape',
         choices: ['Circle', 'Square', 'Triangle'],
     },
 ];
+
+
+async function init() {
+    try {
+        const answers = await inquirer.prompt(questions);
+        console.log(answers)
+        const {text, color, shape} = answers;
+        let shapeReal
+        if (shape  === 'circle'){
+            shapeReal = new Circle();
+             shapeReal.setColor(color);
+        } else if (shape === 'square'){
+            shapeReal = new Square();
+            shapeReal.setColor(color);
+        }else if (shape === 'triangle'){
+            shapeReal = new Triangle();
+            shapeReal.setColor(color);
+        }
+        console.log(shapeReal)
+        let userText
+        if (answers.text.length>0 && answers.text.length<4) {
+            userText = answers.text; 
+        } else {
+            console.log("Invalid user entry, please enter between 1-3 characters")
+            return 
+        }
+        let fontColor = answers.fontColor
+        let shapeColor = answerShape
+
+        let svg = new Svg()
+        svg.setTextElement(userText, fontColor)
+        svg.setShapeElement(shapeReal)
+        let svgRender = svg.render()
+
+        writeToFile('logo.svg', svgRender)
+
+
+        console.log('Successfully created logo.svg');
+    } catch (err) {
+        console.log(err); 
+    }
+}
+
+init()
+
